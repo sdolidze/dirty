ecf76897c188f800dd055b0f88c599a9d322712a
Sandro Dolidze <me@sdolidze.ge>
2017-12-25 15:13:26 +0400

    Play with Global libarary and Nan-s

--- src/Main.purs
+++ src/Main.purs
@@ -4,12 +4,15 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Math (sqrt, pi)
+import Global (nan, isNaN)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
 circleArea :: Number -> Number
 circleArea r = pi * r * r
+
+isNanNan = isNaN nan
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
