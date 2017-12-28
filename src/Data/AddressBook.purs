87da21d19d0e7f403467542c94f0f03717cf53c6
Sandro Dolidze <me@sdolidze.ge>
2017-12-28 18:32:14 +0400

    add address book creation, insertion, search

--- src/Data/AddressBook.purs
+++ src/Data/AddressBook.purs
@@ -2,9 +2,9 @@ module Data.AddressBook where
 
 import Prelude
 
--- import Control.Plus (empty)
+import Control.Plus (empty)
 import Data.List (List(..), filter, head)
--- import Data.Maybe (Maybe)
+import Data.Maybe (Maybe)
 
 type AddressBook = List Entry
 
@@ -17,5 +17,39 @@ type Entry =
 type Address =
     { street :: String
     , city :: String
-    , state :: String
+    , zip :: String
     }
+
+sandro :: Entry
+sandro = {
+    firstName: "Sandro",
+    lastName: "Dolidze",
+    address: {
+        street: "11 Tamarashvili Street",
+        city: "Tbilsi",
+        zip: "0179"
+    }
+}
+
+showEntry :: Entry -> String
+showEntry entry =
+    entry.lastName <> ", " <>
+    entry.firstName <> ": " <>
+    showAddress entry.address
+
+showAddress :: Address -> String
+showAddress address =
+    address.street <> ", " <>
+    address.city <> ", " <>
+    address.zip
+
+emptyBook :: AddressBook
+emptyBook = empty
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry = Cons
+
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName book = 
+    head $
+    filter (\entry -> entry.firstName == firstName && entry.lastName == lastName) book
