d8c5d37568e5013bc18e771aa1aec2bf6f1696ec
Sandro Dolidze <me@sdolidze.ge>
2017-12-29 15:15:21 +0400

    playing with tail recursion

--- src/Chapter4.purs
+++ src/Chapter4.purs
@@ -3,10 +3,12 @@ module Chapter4 where
 import Prelude
 
 import Control.MonadZero (guard)
-import Data.Array (concat, concatMap, null, range)
+import Data.Array (concat, concatMap, null, range, cons)
 import Data.Array.Partial (head, tail)
+import Data.Foldable (foldl, foldr)
 import Data.List as L
 import Data.Tuple (Tuple(..))
+import Math (abs)
 import Partial.Unsafe (unsafePartial)
 
 fact :: Int -> Int
@@ -105,3 +107,34 @@ cartesianProduct xs ys = do
     x <- xs
     y <- ys
     pure $ Tuple x y
+
+pythagoreanTriples :: Int -> Array (Array Int)
+pythagoreanTriples n = do
+  a <- 1 .. n
+  b <- 1 .. n
+  c <- 1 .. n
+  guard $ a * a + b * b == c * c
+  pure [a, b, c]
+
+-- 4.12
+
+sum :: Array Int -> Int
+sum = foldl (+) 0
+
+reverseConcat :: Array Int -> String
+reverseConcat = foldr (\n acc -> acc <> show n) ""
+
+-- 4.13
+
+fact2 :: Int -> Int 
+fact2 n = fact' n 1
+    where
+        fact' 0 acc = acc
+        fact' n acc = fact' (n - 1) (n * acc)
+
+reverse :: forall a. Array a -> Array a
+reverse = reverse' []
+    where
+      reverse' acc [] = acc
+      reverse' acc xs = reverse' (cons (unsafePartial head xs) acc)
+                                 (unsafePartial tail xs)
